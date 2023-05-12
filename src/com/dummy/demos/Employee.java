8f46f800c8aedebc530a5b915fe3dd5b84e89f53
Lata Verma <lata.soni@gmail.com>
2023-05-12 14:45:45 +0530

    Added Employee

---
+++ src/com/dummy/demos/Employee.java
@@ -0,0 +1,35 @@
+package com.dummy.demos;
+
+public class Employee {
+	private int empId;
+	private String empName;
+	private int empSalary;
+	private String empDept;
+	
+	public String getEmpDept() {
+		return empDept;
+	}
+	public void setEmpDept(String empDept) {
+		this.empDept = empDept;
+	}
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public int getEmpSalary() {
+		return empSalary;
+	}
+	public void setEmpSalary(int empSalary) {
+		this.empSalary = empSalary;
+	}
+	
+
+}
