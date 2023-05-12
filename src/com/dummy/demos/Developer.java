99762537d3351c487a5a60214212540ad8fba55f
Lata Verma <lata.soni@gmail.com>
2023-05-12 17:10:23 +0530

    Developer Added

---
+++ src/com/dummy/demos/Developer.java
@@ -0,0 +1,14 @@
+package com.dummy.demos;
+
+public class Developer {
+private String technology;
+
+public String getTechnology() {
+	return technology;
+}
+
+public void setTechnology(String technology) {
+	this.technology = technology;
+}
+
+}
