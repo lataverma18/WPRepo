ef8650d87b8b47fb2f128dd9c169cb2018ac6a91
Lata Verma <lata.soni@gmail.com>
2023-05-12 17:16:48 +0530

    Manager Added

---
+++ src/com/dummy/demos/Manager.java
@@ -0,0 +1,13 @@
+package com.dummy.demos;
+
+public class Manager extends EmployeeDetail {
+	private String department;
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+}
